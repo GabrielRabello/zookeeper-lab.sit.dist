fe6da1da9f3953c62d8ce81463a41025ee39432b
GabrielRabello <gabriel.rabello@dgtallab.com.br>
2025-02-02 12:32:16 -0300

    feature: implementada barreira simples (atividade 2)

--- src/main/java/br/ufpa/SyncPrimitive.java
+++ src/main/java/br/ufpa/SyncPrimitive.java
@@ -1,9 +1,6 @@
 package br.ufpa;
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.time.LocalTime;
-import java.util.List;
 import java.util.Random;
 
 import org.slf4j.Logger;
@@ -42,13 +39,6 @@ public class SyncPrimitive implements Watcher {
             if (!event.getType().equals(Event.EventType.None)) {
                 System.out.println(LocalTime.now()+": Event - " + event.getType() + " Mutex:" + mutex);
             }
-            // Delay in notification to cause deadlock
-            var n = new Random().nextInt(20+1);
-            try {
-                Thread.sleep(n*100);
-            } catch (InterruptedException e) {
-                log.error(e.toString());
-            }
             mutex.notify();
         }
     }
@@ -63,77 +53,57 @@ public class SyncPrimitive implements Watcher {
         /**
          * Barrier constructor
          */
-        Barrier(String address, String root, int size) {
+        Barrier(String address, String root, int size, boolean starter) {
             super(address);
             this.root = root;
             this.size = size;
 
-            // Create barrier node
-            if (zk != null) {
+            // Hypothetical condition that unblocks other processes to make their work
+            if (starter) {
+                System.out.println("Condition met. Creating barrier node...");
+                // Create barrier node
                 try {
-                    Stat s = zk.exists(root, false);
-                    if (s == null) {
-                        zk.create(root, new byte[0], Ids.OPEN_ACL_UNSAFE,
-                                  CreateMode.PERSISTENT);
+                    if (zk.exists(root, true) == null){
+                        zk.create(root, new byte[0], Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+                        System.out.println(LocalTime.now()+": Barrier node created");
                     }
                 } catch (KeeperException e) {
-                    log.error("Keeper exception when instantiating queue: "+ e);
+                    log.error("Keeper exception when instantiating queue: " + e);
                 } catch (InterruptedException e) {
                     log.error("Interrupted exception: " + e);
                 }
+                return;
             }
 
-            // My node name
+            //
             try {
-                this.name = InetAddress.getLocalHost().getCanonicalHostName();
-            } catch (UnknownHostException e) {
-               log.error(e.toString());
+                while (true) {
+                    synchronized (mutex) {
+                        Stat stat = zk.exists(root, true);
+                        if (stat == null) {
+                            // Barrier node does not exist, proceed
+                            System.out.println(LocalTime.now() + ": No barrier. proceeding...");
+                            return;
+                        } else {
+                            // Barrier node exists, wait for the watch event
+                            System.out.println(LocalTime.now() + ": Waiting for barrier node to be removed...");
+                            mutex.wait();
+                        }
+                    }
+                }
+            } catch (KeeperException e) {
+                log.error("Keeper exception when instantiating queue: " + e);
+            } catch (InterruptedException e) {
+                log.error("Interrupted exception: " + e);
             }
         }
 
-        /**
-         * Join barrier. The thread will block here until all processes join the barrier
-         */
-        boolean enter() throws KeeperException, InterruptedException{
-            var createdNode = zk.create(root + "/" + name, new byte[0], Ids.OPEN_ACL_UNSAFE,
-                      CreateMode.EPHEMERAL_SEQUENTIAL);
-            var split = createdNode.split("/");
-            name = split[split.length-1];
-            System.out.println("Created: "+createdNode);
-            while (true) {
-                // Key point where current implementation can enter deadlock.
-                // Between the znode creation and getChildren, one of the nodes may already be deleted,
-                // so list.size() will always be less than size.
-                synchronized (mutex) {
-                    List<String> list = zk.getChildren(root, true);
-                    if (list.size() < size) {
-                        System.out.println(LocalTime.now()+": Waiting to enter the barrier...");
-                        mutex.wait();
-                    } else {
-                        return true;
-                    }
-                }
-            }
-        }
-
-        /**
-         * Wait until all reach barrier
-         */
-        boolean leave() throws KeeperException, InterruptedException{
-            zk.delete(root + "/" + name, 0);
-            System.out.println("Deleted: " + root + "/" + name);
-            System.out.println(LocalTime.now()+": Waiting to leave the barrier");
-            while (true) {
-                synchronized (mutex) {
-                    List<String> list = zk.getChildren(root, true);
-                    System.out.println(LocalTime.now()+": Remaining in Barrier: "+ list + "\n");
-
-                    if (!list.isEmpty()) {
-                        mutex.wait();
-                    } else {
-                        return true;
-                    }
-                }
+        public void RemoveBarrier() {
+            try {
+                zk.delete(root, 0);
+                System.out.println("Barrier node deleted");
+            } catch (InterruptedException | KeeperException e) {
+                log.error(e.toString());
             }
         }
     }
@@ -143,21 +113,20 @@ public class SyncPrimitive implements Watcher {
     }
 
     public static void barrierTest(String[] args) {
-        int size = Integer.parseInt(args[1]);
-        Barrier b = new Barrier(args[0], "/b1", size);
-        System.out.println("Mutex Id: " + SyncPrimitive.mutex);
-        try{
-            boolean flag = b.enter();
-            System.out.println("ALL PROCESSES ("+args[1]+") JOINED BARRIER");
-            if(!flag) System.out.println("Error when entering the barrier");
-        } catch (KeeperException | InterruptedException e){
-            log.error(e.toString());
+        int size = Integer.parseInt(args[1]); // size not actually used in this example
+        boolean isStarter = false;
+        if (args.length == 3) {
+            isStarter = args[2].equalsIgnoreCase("true");
         }
+        var b = new Barrier(args[0], "/b1", size, isStarter);
 
         // SIMULATES SOME WORK
         Random rand = new Random();
-        int r = rand.nextInt(20);
-        System.out.println("WORK WILL TAKE " + (100 * r) / 1000+ "s... I WILL STILL RECEIVE EVENTS BETWEEN TASKS\n");
+        int r = rand.nextInt(100);
+        if (isStarter) {
+            r = 10;
+        }
+        System.out.println("WORK WILL TAKE " + (100 * r) / 1000+ "s...\n");
         for (int i = 0; i < r; i++) {
             try {
                 Thread.sleep(100);
@@ -165,12 +134,11 @@ public class SyncPrimitive implements Watcher {
                 log.error(e.toString());
             }
         }
-        try{
-            var flag = b.leave();
-            if (!flag) System.out.println("Error when leaving the barrier");
-        } catch (KeeperException | InterruptedException e){
-            log.error(e.toString());
+
+        if (isStarter) {
+            b.RemoveBarrier();
         }
-        System.out.println("Left barrier");
+
+        System.out.println(LocalTime.now()+": WORK DONE");
     }
 }
