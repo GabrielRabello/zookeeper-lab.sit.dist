ee11dc27fe8f61d91ad089620ae206bdc4f8db04
GabrielRabello <gabriel.rabello@dgtallab.com.br>
2025-02-02 01:27:10 -0300

    refactor: removido código de fila (producer e consumer). Já que o foco é barreiras

--- src/main/java/br/ufpa/SyncPrimitive.java
+++ src/main/java/br/ufpa/SyncPrimitive.java
@@ -138,142 +138,17 @@ public class SyncPrimitive implements Watcher {
         }
     }
 
-    /**
-     * Producer-Consumer queue
-     */
-    static public class Queue extends SyncPrimitive {
-
-        /**
-         * Constructor of producer-consumer queue
-         *
-         * @param address
-         * @param name
-         */
-        Queue(String address, String name) {
-            super(address);
-            this.root = name;
-            // Create ZK node name
-            if (zk != null) {
-                try {
-                    Stat s = zk.exists(root, false);
-                    if (s == null) {
-                        zk.create(root, new byte[0], Ids.OPEN_ACL_UNSAFE,
-                                  CreateMode.PERSISTENT);
-                    }
-                } catch (KeeperException e) {
-                    System.out
-                            .println("Keeper exception when instantiating queue: "
-                                             + e.toString());
-                } catch (InterruptedException e) {
-                    System.out.println("Interrupted exception");
-                }
-            }
-        }
-
-        /**
-         * Add element to the queue.
-         */
-        boolean produce(int i) throws KeeperException, InterruptedException{
-            ByteBuffer b = ByteBuffer.allocate(4);
-            byte[] value;
-
-            // Add child with value i
-            b.putInt(i);
-            value = b.array();
-            zk.create(root + "/element", value, Ids.OPEN_ACL_UNSAFE,
-                      CreateMode.PERSISTENT_SEQUENTIAL);
-
-            return true;
-        }
-
-
-        /**
-         * Remove first element from the queue.
-         */
-        int consume() throws KeeperException, InterruptedException{
-            int retvalue = -1;
-
-            // Get the first element available
-            while (true) {
-                synchronized (mutex) {
-                    List<String> list = zk.getChildren(root, true);
-                    if (list.isEmpty()) {
-                        System.out.println("Going to wait");
-                        mutex.wait();
-                    } else {
-                        int min;
-                        try {
-                            min = Integer.parseInt(list.getFirst().substring(7));
-                            for (String s : list) {
-                                int tempValue = Integer.parseInt(s.substring(7));
-                                //System.out.println("Temporary value: " + tempValue);
-                                if (tempValue < min) min = tempValue;
-                            }
-                        } catch (NumberFormatException e) {
-                            log.error("e: ", e);
-                            return -1;
-                        }
-                        System.out.println("Temporary value: " + root + "/element" + min);
-                        byte[] b = zk.getData(root + "/element" + min,
-                                              false, null);
-                        zk.delete(root + "/element" + min, 0);
-                        ByteBuffer buffer = ByteBuffer.wrap(b);
-                        retvalue = buffer.getInt();
-
-                        return retvalue;
-                    }
-                }
-            }
-        }
-    }
-
     public static void main(String[] args) {
-        if (args[0].equals("qTest"))
-            queueTest(args);
-        else
-            barrierTest(args);
-    }
-
-    public static void queueTest(String[] args) {
-        Queue q = new Queue(args[1], "/app1");
-
-        System.out.println("Input: " + args[1]);
-        int i;
-        int max = Integer.parseInt(args[2]);
-        var role = args[3];
-
-        if (role.equals("p")) {
-            System.out.println("Producer");
-            for (i = 0; i < max; i++)
-                try{
-                    q.produce(10 + i);
-                } catch (KeeperException | InterruptedException e){
-                    log.error("e: ", e);
-                }
-        } else {
-            System.out.println("Consumer");
-
-            for (i = 0; i < max; i++) {
-                try{
-                    int r = q.consume();
-                    System.out.println("Item: " + r);
-                } catch (KeeperException e){
-                    log.warn("e: ", e);
-                    i--;
-                } catch (InterruptedException e){
-                    log.error("e: ", e);
-                }
-            }
-        }
+        barrierTest(args);
     }
 
     public static void barrierTest(String[] args) {
-        int size = Integer.parseInt(args[2]);
-        Barrier b = new Barrier(args[1], "/b1", size);
+        int size = Integer.parseInt(args[1]);
+        Barrier b = new Barrier(args[0], "/b1", size);
         System.out.println("Mutex Id: " + SyncPrimitive.mutex);
         try{
             boolean flag = b.enter();
-            System.out.println("ALL PROCESSES ("+args[2]+") JOINED BARRIER");
+            System.out.println("ALL PROCESSES ("+args[1]+") JOINED BARRIER");
             if(!flag) System.out.println("Error when entering the barrier");
         } catch (KeeperException | InterruptedException e){
             log.error(e.toString());
