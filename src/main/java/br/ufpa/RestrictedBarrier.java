18e49f9b65f3c5a068485a4ea4309839987f98be
GabrielRabello <gabriel.rabello@dgtallab.com.br>
2025-02-16 22:59:21 -0300

    add barreira restrita

---
+++ src/main/java/br/ufpa/RestrictedBarrier.java
@@ -0,0 +1,135 @@
+package br.ufpa;
+
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.time.LocalTime;
+import java.util.List;
+
+public class RestrictedBarrier extends SyncPrimitive implements IBarrier {
+    int size;
+    String name;
+    final String subsetId;
+    final String subsetPath;
+    final String readyNode;
+    String nodePath;
+
+    /**
+     * Barrier constructor
+     */
+    RestrictedBarrier(String address, String root, String subsetId, int size) {
+        super(address);
+        this.root = root;
+        this.size = size;
+        this.subsetId = subsetId;
+        this.subsetPath = root + "/" + subsetId;
+        this.readyNode = this.subsetPath+ "/ready";
+
+        // Create barrier node
+        if (zk != null) {
+            try {
+                if (zk.exists(root, false) == null) {
+                    zk.create(root, new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                              CreateMode.PERSISTENT);
+                }
+                if (zk.exists(subsetPath, false) == null) {
+                    zk.create(subsetPath, new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                              CreateMode.PERSISTENT);
+                }
+            } catch (KeeperException e) {
+                log.error("Keeper exception when instantiating queue: "+ e);
+            } catch (InterruptedException e) {
+                log.error("Interrupted exception: " + e);
+            }
+        }
+
+        // My node name
+        try {
+            this.name = InetAddress.getLocalHost().getCanonicalHostName();
+        } catch (UnknownHostException e) {
+            log.error(e.toString());
+        }
+        this.nodePath = subsetPath + "/" + name;
+    }
+
+    public boolean enter() throws KeeperException, InterruptedException {
+        // Step 2: Set watch: exists(b + "/ready", true)
+        zk.exists(this.readyNode, true);
+
+        // Step 1 and 3: Create a name and child: create(n, EPHEMERAL) (the generated name is using sequential feature)
+        var znode = zk.create(nodePath, new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+        System.out.println("Created: "+znode);
+        var split = znode.split("/");
+        name = split[split.length-1];
+        this.nodePath = subsetPath + "/" + name;
+
+        try {
+            while (true) {
+                synchronized (mutex) {
+                    // Step 4: L = getChildren(b, false)
+                    List<String> children = zk.getChildren(subsetPath, false);
+
+                    // Step 5: if fewer children in L than x, wait for watch event
+                    if (children.size() < size) {
+                        System.out.println(LocalTime.now() + ": Waiting for more processes to enter the barrier...");
+                        mutex.wait();
+                    } else {
+                        // Step 6: else create(b + "/ready", REGULAR). Last process to join barrier creates the ready node
+                        if (zk.exists(readyNode, true) == null) {
+                            var readyZNode = zk.create(readyNode, new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+                            System.out.println("Created: "+readyZNode);
+                        }
+                        return true;
+                    }
+                }
+            }
+        } catch (KeeperException | InterruptedException e) {
+            log.error(e.toString());
+        }
+
+        return false;
+    }
+
+    public boolean leave() throws KeeperException, InterruptedException {
+        System.out.println(LocalTime.now()+": Waiting to leave the barrier");
+        while (true) {
+            synchronized (mutex) {
+                // Step 1: L = getChildren(b, false)
+                List<String> children = zk.getChildren(subsetPath, false);
+                System.out.println(LocalTime.now() + ": Remaining in Barrier: " + children + "\n");
+                children.remove("ready");
+
+                // Step 2: if no children, exit
+                if (children.isEmpty()) {
+                    return true;
+                }
+
+                // Step 3: if p is only process node in L, delete(n) and exit
+                if (children.size() == 1) {
+                    System.out.println("Deleting barrier node");
+                    zk.delete(nodePath, 0);
+                    zk.delete(readyNode, -1);
+                    return true;
+                }
+
+                // Step 4: if p is the lowest process node in L, wait on highest process node in L
+                children.sort(String::compareTo);
+                if (children.getFirst().equals(name)) {
+                    String highestNode = children.getLast();
+                    zk.exists(subsetPath+ "/"+ highestNode, true);
+                    mutex.wait();
+                } else {
+                    // Step 5: else delete(n) if still exists and wait on lowest process node in L
+                    zk.delete(nodePath, 0);
+                    System.out.println("Deleted: " + nodePath);
+                    String lowestNode = children.getFirst();
+                    zk.exists(subsetPath + "/"+lowestNode, true);
+                    mutex.wait();
+                }
+            }
+        }
+    }
+}
--- src/main/java/br/ufpa/SyncPrimitive.java
+++ src/main/java/br/ufpa/SyncPrimitive.java
@@ -18,9 +18,9 @@ import org.apache.zookeeper.ZooDefs.Ids;
 // This was based on pseudo-code from: https://zookeeper.apache.org/doc/r3.6.3/recipes.html
 public class SyncPrimitive implements Watcher {
 
-    private static final Logger log = LoggerFactory.getLogger(SyncPrimitive.class);
+    protected static final Logger log = LoggerFactory.getLogger(SyncPrimitive.class);
     static ZooKeeper zk = null;
-    private static final Object mutex = new Object();
+    protected static final Object mutex = new Object();
 
     String root;
 
@@ -42,13 +42,6 @@ public class SyncPrimitive implements Watcher {
             if (!event.getType().equals(Event.EventType.None)) {
                 System.out.println(LocalTime.now()+": Event - " + event.getType() + " Mutex:" + mutex);
             }
-            // Delay in notification to TRY to cause deadlock (will not happen with this implementation)
-            var n = new Random().nextInt(20+1);
-            try {
-                Thread.sleep(n*100);
-            } catch (InterruptedException e) {
-                log.error(e.toString());
-            }
             mutex.notify();
         }
     }
@@ -61,130 +54,23 @@ public class SyncPrimitive implements Watcher {
         }
     }
 
-    /**
-     * Barrier
-     */
-    static public class Barrier extends SyncPrimitive {
-        int size;
-        String name;
-        static final String readyNode = "ready";
-
-        /**
-         * Barrier constructor
-         */
-        Barrier(String address, String root, int size) {
-            super(address);
-            this.root = root;
-            this.size = size;
-
-            // Create barrier node
-            if (zk != null) {
-                try {
-                    if (zk.exists(root, false) == null) {
-                        zk.create(root, new byte[0], Ids.OPEN_ACL_UNSAFE,
-                                  CreateMode.PERSISTENT);
-                    }
-                } catch (KeeperException e) {
-                    log.error("Keeper exception when instantiating queue: "+ e);
-                } catch (InterruptedException e) {
-                    log.error("Interrupted exception: " + e);
-                }
-            }
-
-            // My node name
-            try {
-                this.name = InetAddress.getLocalHost().getCanonicalHostName();
-            } catch (UnknownHostException e) {
-               log.error(e.toString());
-            }
-        }
-
-        public boolean enter() throws KeeperException, InterruptedException {
-            // Step 2: Set watch: exists(b + "/ready", true)
-            zk.exists(root + "/"+readyNode, true);
-
-            // Step 1 and 3: Create a name and child: create(n, EPHEMERAL) (the generated name is using sequential feature)
-            var znode = zk.create(root +"/"+name, new byte[0], Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
-            System.out.println("Created: "+znode);
-            var split = znode.split("/");
-            name = split[split.length-1];
-
-            try {
-                while (true) {
-                    synchronized (mutex) {
-                        // Step 4: L = getChildren(b, false)
-                        List<String> children = zk.getChildren(root, false);
-
-                        // Step 5: if fewer children in L than x, wait for watch event
-                        if (children.size() < size) {
-                            System.out.println(LocalTime.now() + ": Waiting for more processes to enter the barrier...");
-                            mutex.wait();
-                        } else {
-                            // Step 6: else create(b + "/ready", REGULAR). Last process to join barrier creates the ready node
-                            if (zk.exists(root + "/"+readyNode, true) == null) {
-                                var readyZNode = zk.create(root + "/"+readyNode, new byte[0], Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
-                                System.out.println("Created: "+readyZNode);
-                            }
-                            return true;
-                        }
-                    }
-                }
-            } catch (KeeperException | InterruptedException e) {
-                log.error(e.toString());
-            }
-
-            return false;
-        }
-
-        public boolean leave() throws KeeperException, InterruptedException {
-            System.out.println(LocalTime.now()+": Waiting to leave the barrier");
-            while (true) {
-                synchronized (mutex) {
-                    // Step 1: L = getChildren(b, false)
-                    List<String> children = zk.getChildren(root, false);
-                    System.out.println(LocalTime.now() + ": Remaining in Barrier: " + children + "\n");
-                    children.remove(readyNode);
-
-                    // Step 2: if no children, exit
-                    if (children.isEmpty()) {
-                        return true;
-                    }
-
-                    // Step 3: if p is only process node in L, delete(n) and exit
-                    if (children.size() == 1) {
-                        System.out.println("Deleting barrier node");
-                        zk.delete(root+"/"+name, 0);
-                        zk.delete(root+"/"+readyNode, 0);
-                        return true;
-                    }
-
-                    // Step 4: if p is the lowest process node in L, wait on highest process node in L
-                    children.sort(String::compareTo);
-                    if (children.getFirst().equals(name)) {
-                        String highestNode = children.getLast();
-                        zk.exists(root + "/" + highestNode, true);
-                        mutex.wait();
-                    } else {
-                        // Step 5: else delete(n) if still exists and wait on lowest process node in L
-                        zk.delete(root + "/" + name, 0);
-                        System.out.println("Deleted: "+root + "/" + name);
-                        String lowestNode = children.getFirst();
-                        zk.exists(root + "/" + lowestNode, true);
-                        mutex.wait();
-                    }
-                }
-            }
-        }
-    }
-
     public static void main(String[] args) {
         String root = args[0];
         int size = Integer.parseInt(args[1]);
-        barrierTest(root, size);
+        int barrierType = Integer.parseInt(args[2]);
+        var subsetId = args[3];
+        final var barrierRoot = "/b1";
+        switch (barrierType) {
+            case 2:
+                barrierTest(root, size, new RestrictedBarrier(root, barrierRoot, subsetId, size));
+                break;
+            default:
+                System.out.println("Invalid barrier type");
+                System.exit(-1);
+        }
     }
 
-    public static void barrierTest(String root, int size) {
-        Barrier b = new Barrier(root, "/b1", size);
+    public static void barrierTest(String root, int size, IBarrier b) {
         try{
             boolean flag = b.enter();
             System.out.println("ALL PROCESSES ("+size+") JOINED BARRIER");
